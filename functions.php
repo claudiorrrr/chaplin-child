78e006514045a194523f224995abc278d69ed5a9
Hugo Solar <hugo.solar@gmail.com>
2020-12-23 09:43:33 -0300

    add local css file as a child from parent style

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+   wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ), wp_get_theme()->get('Version') );
 }
 
 //* Adding support to SVG
